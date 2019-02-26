1cd0537acbbc036c7b44e745c09c66afb119ff30
satom9to5 <satom9to5@gmail.com>
2019-02-26 23:51:52 +0900

    fix tests

--- nativemessaging/nativemessaging_test.go
+++ nativemessaging/nativemessaging_test.go
@@ -3,29 +3,24 @@ package nativemessaging
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
 	"strings"
 	"testing"
 )
 
 type TestStruct struct {
-	Name string
+	Name string `json:"name"`
 }
 
 func TestReceive(t *testing.T) {
-	testJsonStr := `
-{ "name": "Receive" }
-`
-
-	buffer := new(bytes.Buffer)
-	err := binary.Write(buffer, binary.LittleEndian, uint32(len(testJsonStr)))
-	if err != nil {
-		t.Fatalf("Fail write header length")
-	}
+	// test json: {"name":"Receive"}
+	bs := []byte{18, 0, 0, 0, 123, 34, 110, 97, 109, 101, 34, 58, 34, 82, 101, 99, 101, 105, 118, 101, 34, 125}
+	buffer := bytes.NewBuffer(bs)
 
 	receiveTarget := &TestStruct{}
-	buffer.Write([]byte(testJsonStr))
-	err = Receive(receiveTarget, buffer)
+
+	if err := Receive(receiveTarget, buffer); err != nil {
+		t.Fatalf("Fail Receive. err: %s", err)
+	}
 
 	if receiveTarget.Name != "Receive" {
 		t.Fatalf("Expected: Receive, got: %s", receiveTarget.Name)
@@ -34,25 +29,25 @@ func TestReceive(t *testing.T) {
 
 func TestSend(t *testing.T) {
 	testSendStruct := &TestStruct{Name: "Send"}
+	exceptJsonStr := `{"name":"Send"}`
+	exceptJsonStrLength := uint32(len(exceptJsonStr))
 
 	buffer := new(bytes.Buffer)
 	if err := Send(testSendStruct, buffer); err != nil {
-		t.Fatalf("Fail send")
+		t.Fatalf("Fail Send. err: %s", err)
 	}
-
-	jsonData, _ := json.Marshal(testSendStruct)
 
 	length := uint32(0)
 	if err := binary.Read(buffer, binary.LittleEndian, &length); err != nil {
-		t.Fatal("Read header error")
+		t.Fatalf("Read header error. header length: %d", length)
 	}
 	// header length check
-	if length != uint32(len(jsonData)) {
-		t.Fatalf("header length is incorrect. header length: %d, json length: %d", length, len(jsonData))
+	if length != exceptJsonStrLength {
+		t.Fatalf("header length is incorrect. header length: %d, json length: %d", length, exceptJsonStrLength)
 	}
 
 	// contents check
-	if strings.Index(buffer.String(), string(jsonData)) < 0 {
+	if strings.Index(buffer.String(), exceptJsonStr) < 0 {
 		t.Fatalf("Unexpected return")
 	}
 }
